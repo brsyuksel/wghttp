de25fbf35a65ade35f3ea518f88b0a78526f64e9
Baris Yuksel <brsyuksel@gmail.com>
2022-12-01 16:18:18 +0000

    impl interface manager with libc, tests

---
+++ if_mng_libc/src/lib.rs
@@ -0,0 +1,158 @@
+use core::if_mng::*;
+use libc;
+use std::mem::MaybeUninit;
+use std::net::Ipv4Addr;
+
+#[cfg(test)]
+mod tests;
+
+extern "C" {
+    fn inet_addr(cp: *const libc::c_char) -> libc::in_addr_t;
+}
+
+pub struct InterfaceManagerLibC<'a> {
+    device_name: &'a str,
+}
+
+impl<'a> InterfaceManagerLibC<'a> {
+    pub fn new(dev_name: &'a str) -> Self {
+        Self {
+            device_name: dev_name,
+        }
+    }
+}
+
+impl<'a> InterfaceManager for InterfaceManagerLibC<'a> {
+    fn get_device_name(&self) -> &str {
+        self.device_name
+    }
+
+    fn up_device(&self) -> Result<(), InterfaceError> {
+        unsafe {
+            DeviceCommand::Up(self.get_device_name().to_owned())
+                .exec()
+                .map_err(|e| InterfaceError(e))
+        }
+    }
+
+    fn set_ip_and_netmask(
+        &self,
+        ip_addr: &Ipv4Addr,
+        netmask: &Ipv4Addr,
+    ) -> Result<(), InterfaceError> {
+        unsafe {
+            DeviceCommand::SetIp(self.get_device_name().to_owned(), ip_addr)
+                .exec()
+                .map_err(|e| InterfaceError(e))?;
+
+            DeviceCommand::SetNetmask(self.get_device_name().to_owned(), netmask)
+                .exec()
+                .map_err(|e| InterfaceError(e))
+        }
+    }
+}
+
+enum DeviceCommand<'a> {
+    SetIp(String, &'a Ipv4Addr),
+    SetNetmask(String, &'a Ipv4Addr),
+    Up(String),
+}
+
+impl<'a> DeviceCommand<'a> {
+    unsafe fn open_control_socket() -> Result<i32, String> {
+        let s = libc::socket(libc::AF_INET, libc::SOCK_DGRAM, 0);
+        if s < 0 {
+            return Err(String::from("control socket problem"));
+        }
+        Ok(s)
+    }
+
+    unsafe fn close_control_socket(fd: i32) {
+        libc::close(fd);
+    }
+
+    unsafe fn set_ip(fd: i32, dev_name: &str, ip_addr: &str) -> Result<(), String> {
+        let ifreq = MaybeUninit::<libc::ifreq>::uninit().as_mut_ptr();
+
+        let mut dev_name_arr = [0u8; 16];
+        dev_name_arr[..dev_name.len()].copy_from_slice(dev_name.as_bytes());
+        (*ifreq).ifr_name = dev_name_arr;
+
+        let ip_sin = libc::sockaddr_in {
+            sin_family: libc::AF_INET as u16,
+            sin_port: 0,
+            sin_addr: libc::in_addr {
+                s_addr: inet_addr(ip_addr.to_owned().as_ptr()),
+            },
+            sin_zero: [0u8; 8],
+        };
+        (*ifreq).ifr_ifru.ifru_addr = *(&ip_sin as *const _ as *const libc::sockaddr);
+
+        if libc::ioctl(fd, libc::SIOCSIFADDR, ifreq) < 0 {
+            return Err(String::from("can't set ip address for device"));
+        }
+
+        Ok(())
+    }
+
+    unsafe fn set_netmask(fd: i32, dev_name: &str, netmask: &str) -> Result<(), String> {
+        let ifreq = MaybeUninit::<libc::ifreq>::uninit().as_mut_ptr();
+
+        let mut dev_name_arr = [0u8; 16];
+        dev_name_arr[..dev_name.len()].copy_from_slice(dev_name.as_bytes());
+        (*ifreq).ifr_name = dev_name_arr;
+
+        let netmask_sin = libc::sockaddr_in {
+            sin_family: libc::AF_INET as u16,
+            sin_port: 0,
+            sin_addr: libc::in_addr {
+                s_addr: inet_addr(netmask.to_owned().as_ptr()),
+            },
+            sin_zero: [0u8; 8],
+        };
+        (*ifreq).ifr_ifru.ifru_netmask = *(&netmask_sin as *const _ as *const libc::sockaddr);
+
+        if libc::ioctl(fd, libc::SIOCSIFNETMASK, ifreq) < 0 {
+            return Err(String::from("can't set netmask for device"));
+        }
+
+        Ok(())
+    }
+
+    unsafe fn up_device(fd: i32, dev_name: &str) -> Result<(), String> {
+        let ifreq = MaybeUninit::<libc::ifreq>::uninit().as_mut_ptr();
+
+        let mut dev_name_arr = [0u8; 16];
+        dev_name_arr[..dev_name.len()].copy_from_slice(dev_name.as_bytes());
+        (*ifreq).ifr_name = dev_name_arr;
+
+        if libc::ioctl(fd, libc::SIOCGIFFLAGS, ifreq) < 0 {
+            return Err(String::from("can't get device flags"));
+        }
+
+        (*ifreq).ifr_ifru.ifru_flags |= libc::IFF_UP as i16;
+
+        if libc::ioctl(fd, libc::SIOCSIFFLAGS, ifreq) < 0 {
+            return Err(String::from("can't set device up"));
+        }
+
+        Ok(())
+    }
+
+    unsafe fn exec(&self) -> Result<(), String> {
+        let fd = Self::open_control_socket()?;
+
+        let res = match self {
+            Self::Up(dev_name) => Self::up_device(fd, dev_name.as_str()),
+            Self::SetIp(dev_name, ip_addr) => {
+                Self::set_ip(fd, dev_name.as_str(), ip_addr.to_string().as_str())
+            }
+            Self::SetNetmask(dev_name, netmask) => {
+                Self::set_netmask(fd, dev_name.as_str(), netmask.to_string().as_str())
+            }
+        };
+
+        Self::close_control_socket(fd);
+        res
+    }
+}
---
+++ if_mng_libc/src/tests.rs
@@ -0,0 +1,158 @@
+mod test_helper {
+    use std::process::Command;
+
+    pub fn manage_dev<F>(name: &str, fcall: F)
+    where
+        F: FnOnce() -> (),
+    {
+        let create_cmd = format!("link add dev {name} type wireguard");
+        let create = Command::new("ip")
+            .args(create_cmd.split_whitespace())
+            .output();
+        assert!(create.err().is_none());
+
+        fcall();
+
+        let del_cmd = format!("link del dev {name}");
+        let del = Command::new("ip").args(del_cmd.split_whitespace()).output();
+        assert!(del.err().is_none());
+    }
+
+    pub fn ip_addr_output(name: &str) -> String {
+        let cmd = format!("addr show dev {name}");
+        let show = Command::new("ip")
+            .args(cmd.split_whitespace())
+            .output()
+            .expect("cant run ip addr cmd");
+
+        std::str::from_utf8(&show.stdout.as_slice())
+            .unwrap()
+            .to_owned()
+    }
+}
+
+#[cfg(test)]
+mod if_mng_tests {
+    use super::{super::InterfaceManagerLibC, test_helper};
+    use core::if_mng::*;
+
+    #[test]
+    fn fails_on_up_non_existing_device() {
+        let m = InterfaceManagerLibC::new("ne0");
+        let res = m.up_device().map_err(|e| e.0).err();
+        assert_eq!(res, Some("can't get device flags".to_owned()));
+    }
+
+    #[test]
+    fn sets_up_device() {
+        test_helper::manage_dev("iftest0", || {
+            let m = InterfaceManagerLibC::new("iftest0");
+            let res = m.up_device().err();
+            assert!(res.is_none());
+
+            let ip_addr_res = test_helper::ip_addr_output("iftest0");
+            let down = ip_addr_res.contains("DOWN");
+            assert!(!down)
+        });
+    }
+
+    #[test]
+    fn fails_setting_ip_and_netmask_non_existing_device() {
+        let m = InterfaceManagerLibC::new("ne0");
+        let ip = "10.0.0.1".parse().unwrap();
+        let netmask = "255.255.255.0".parse().unwrap();
+        let res = m.set_ip_and_netmask(&ip, &netmask).map_err(|e| e.0).err();
+        assert_eq!(res, Some("can't set ip address for device".to_owned()));
+    }
+
+    #[test]
+    fn sets_ip_and_netmask() {
+        test_helper::manage_dev("iftest0", || {
+            let m = InterfaceManagerLibC::new("iftest0");
+            let ip = "10.0.0.1".parse().unwrap();
+            let netmask = "255.255.255.0".parse().unwrap();
+            let res = m.set_ip_and_netmask(&ip, &netmask).err();
+            assert!(res.is_none());
+
+            let ip_addr_res = test_helper::ip_addr_output("iftest0");
+            let ip10001_24 = ip_addr_res.contains("10.0.0.1/24");
+            assert!(ip10001_24)
+        });
+    }
+}
+
+#[cfg(test)]
+mod device_command_tests {
+    use super::{super::DeviceCommand, test_helper};
+
+    #[test]
+    fn up_command_fails_non_existing_device() {
+        let cmd = DeviceCommand::Up("ne0".to_owned());
+        let res = unsafe { cmd.exec().err() };
+        assert_eq!(res, Some("can't get device flags".to_owned()));
+    }
+
+    #[test]
+    fn sets_up_device() {
+        test_helper::manage_dev("devcmd0", || {
+            let cmd = DeviceCommand::Up("devcmd0".to_owned());
+            let res = unsafe { cmd.exec().err() };
+            assert!(res.is_none());
+
+            let ip_addr_res = test_helper::ip_addr_output("devcmd0");
+            let down = ip_addr_res.contains("DOWN");
+            assert!(!down)
+        });
+    }
+
+    #[test]
+    fn fails_setting_ip_non_existing_device() {
+        let ip = "10.0.0.1".parse().unwrap();
+        let cmd = DeviceCommand::SetIp("ne0".to_owned(), &ip);
+        let res = unsafe { cmd.exec().err() };
+        assert_eq!(res, Some("can't set ip address for device".to_owned()));
+    }
+
+    #[test]
+    fn sets_ip() {
+        test_helper::manage_dev("devcmd1", || {
+            let ip = "10.0.0.1".parse().unwrap();
+            let cmd = DeviceCommand::SetIp("devcmd1".to_owned(), &ip);
+            let res = unsafe { cmd.exec().err() };
+            assert!(res.is_none());
+
+            let ip_addr_res = test_helper::ip_addr_output("devcmd1");
+            let ip10001_32 = ip_addr_res.contains("10.0.0.1/32");
+            assert!(ip10001_32)
+        });
+    }
+
+    #[test]
+    fn fails_setting_netmask_non_existing_device() {
+        let netmask = "255.255.255.0".parse().unwrap();
+        let cmd = DeviceCommand::SetNetmask("ne0".to_owned(), &netmask);
+        let res = unsafe { cmd.exec().err() };
+        assert_eq!(res, Some("can't set netmask for device".to_owned()));
+    }
+
+    #[test]
+    fn sets_netmask() {
+        test_helper::manage_dev("devcmd2", || {
+            let ip = "10.0.0.1".parse().unwrap();
+            let netmask = "255.255.255.0".parse().unwrap();
+            let ip_cmd = DeviceCommand::SetIp("devcmd2".to_owned(), &ip);
+            let netmask_cmd = DeviceCommand::SetNetmask("devcmd2".to_owned(), &netmask);
+
+            let res = unsafe {
+                let _ = ip_cmd.exec();
+                netmask_cmd.exec().err()
+            };
+
+            assert!(res.is_none());
+
+            let ip_addr_res = test_helper::ip_addr_output("devcmd2");
+            let ip10001_24 = ip_addr_res.contains("10.0.0.1/24");
+            assert!(ip10001_24)
+        });
+    }
+}
