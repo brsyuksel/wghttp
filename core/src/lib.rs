56fc22a8bef045ef1ab7dfa56ab7c9d7660405b7
Baris Yuksel <brsyuksel@gmail.com>
2023-01-23 16:28:01 +0000

    initial core wireguard manager def

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod if_mng;
+pub mod wg_mng;
---
+++ core/src/wg_mng/mod.rs
@@ -0,0 +1,26 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct WireguardError(pub String);
+
+impl fmt::Display for WireguardError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "WireguardError: {}", self.0)
+    }
+}
+
+impl std::error::Error for WireguardError {}
+
+#[derive(Debug)]
+pub struct WGDevice {
+    pub name: String,
+    pub public_key: String,
+    pub private_key: String,
+    pub port: u16,
+    pub total_peers: u64,
+}
+
+pub trait WireguardManager {
+    fn add_new_device(&self, device_name: &str, port: u16) -> Result<WGDevice, WireguardError>;
+    fn del_device(&self, device_name: &str) -> Result<(), WireguardError>;
+}
