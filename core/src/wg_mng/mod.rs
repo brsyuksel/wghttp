18059c9ca9e9edff4561a4b4f16f73fd23518da0
Baris Yuksel <brsyuksel@gmail.com>
2023-01-25 21:03:48 +0000

    wgpeer definition, method defs

--- core/src/wg_mng/mod.rs
+++ core/src/wg_mng/mod.rs
@@ -20,9 +20,24 @@ pub struct WGDevice {
     pub total_peers: u64,
 }
 
+#[derive(Debug)]
+pub struct WGPeer {
+    pub public_key: String,
+    pub allowed_ips: Vec<String>,
+    pub persistent_keepalive_interval: u16,
+    pub endpoint: String,
+    pub last_handshake_time: i64,
+    pub rx: u64,
+    pub tx: u64,
+}
+
 pub trait WireguardManager {
     fn add_device(&self, device_name: &str, port: u16) -> Result<WGDevice, WireguardError>;
     fn del_device(&self, device_name: &str) -> Result<(), WireguardError>;
     fn get_device(&self, device_name: &str) -> Result<WGDevice, WireguardError>;
     fn list_devices(&self) -> Result<Vec<WGDevice>, WireguardError>;
+
+    fn add_peer(&self, device_name: &str, allowed_ips: Vec<String>, keepalive: u16) -> Result<(WGPeer, String), WireguardError>;
+    fn del_peer(&self, device_name: &str, public_key: &str) -> Result<(), WireguardError>;
+    fn list_peers(&self, device_name: &str) -> Result<Vec<WGPeer>, WireguardError>;
 }
