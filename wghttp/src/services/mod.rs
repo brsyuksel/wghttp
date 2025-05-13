79916875363393e9f6c78bc0b9a5412de00146c6
Baris Yuksel <brsyuksel@gmail.com>
2025-05-13 22:45:16 +0000

    netdev adapter param for tunnel manager

--- wghttp/src/services/mod.rs
+++ wghttp/src/services/mod.rs
@@ -1,15 +1,25 @@
-use std::sync::Arc;
+use domain::adapters::netdev::NetworkDeviceAdapter;
 use domain::adapters::wg::WireguardAdapter;
+use std::sync::Arc;
 
 #[derive(Clone)]
 pub struct TunnelManager {
     pub wireguard: Arc<dyn WireguardAdapter>,
+    pub netdev: Arc<dyn NetworkDeviceAdapter>,
 }
 
 impl TunnelManager {
-    pub fn new<W: WireguardAdapter + 'static>(wg_adapter: W) -> Self {
+    pub fn new<W, N>(wg_adapter: W, netdev_adapter: N) -> Self
+    where
+        W: WireguardAdapter + 'static,
+        N: NetworkDeviceAdapter + 'static,
+    {
         let wg_arc = Arc::new(wg_adapter);
+        let nd_arc = Arc::new(netdev_adapter);
 
-        TunnelManager { wireguard: wg_arc }
+        TunnelManager {
+            wireguard: wg_arc,
+            netdev: nd_arc,
+        }
     }
 }
