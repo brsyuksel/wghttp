7f614eef64b9b928c7dde34b4c0286ddceb1bcaf
Baris Yuksel <brsyuksel@gmail.com>
2025-05-13 22:46:34 +0000

    input validation, ip managing for devices

--- wghttp/src/routes/devices.rs
+++ wghttp/src/routes/devices.rs
@@ -1,7 +1,9 @@
+use crate::helpers::*;
 use crate::models::devices::*;
 use crate::models::errors::Error;
 use crate::services::TunnelManager;
 use actix_web::{HttpResponse, Responder, delete, get, post, web};
+use domain::models::netdev::NetDevIp;
 
 #[utoipa::path(
     get,
@@ -40,6 +42,7 @@ async fn list_devices(tm: web::Data<TunnelManager>) -> impl Responder {
         (status = 201, description = "device created successfully", body = CreateDeviceResponse),
         (status = 400, description = "validation error", body = Error),
         (status = 409, description = "conflict error", body = Error),
+        (status = 500, description = "system error", body = Error),
     )
 )]
 #[post("/devices")]
@@ -47,28 +50,79 @@ async fn create_device(
     tm: web::Data<TunnelManager>,
     device: web::Json<CreateDeviceRequest>,
 ) -> impl Responder {
-    // TODO: input validation
+    if device.device_name.len() > DEVICE_NAME_MAX_LEN {
+        return HttpResponse::BadRequest().json(Error {
+            message: "device name must be at most 15 characters".to_owned(),
+        });
+    }
+
+    if device.ip_addresses.ipv4.is_none() && device.ip_addresses.ipv6.is_none() {
+        return HttpResponse::BadRequest().json(Error {
+            message: "you must provide at least one of ipv4 or ipv6".to_owned(),
+        });
+    }
+
+    let parsed_ipv4 = device
+        .ip_addresses
+        .ipv4
+        .as_ref()
+        .map(|s| parse_ip(s))
+        .transpose()
+        .map_err(|e| Error { message: e });
+
+    let ipv4 = match parsed_ipv4 {
+        Ok(opt_ip) => opt_ip,
+        Err(e) => return HttpResponse::BadRequest().json(e),
+    };
+
+    let parsed_ipv6 = device
+        .ip_addresses
+        .ipv6
+        .as_ref()
+        .map(|s| parse_ip(s))
+        .transpose()
+        .map_err(|e| Error { message: e });
+
+    let ipv6 = match parsed_ipv6 {
+        Ok(opt_ip) => opt_ip,
+        Err(e) => return HttpResponse::BadRequest().json(e),
+    };
 
     let manager = tm.get_ref();
-    let result = manager
+
+    let wg_result = manager
         .wireguard
         .create_device(&device.device_name, device.port);
-    match result {
-        Err(e) => HttpResponse::Conflict().json(Error { message: e.0 }),
-        Ok(d) => {
-            let dev = CreateDeviceResponse {
-                device_name: d.name,
-                port: d.port,
-                ip_addresses: DeviceIpAddr {
-                    ipv4: None,
-                    ipv6: None,
-                },
-                private_key: d.private_key,
-                public_key: d.public_key,
-            };
-            HttpResponse::Created().json(dev)
-        }
+    if let Err(e) = wg_result {
+        return HttpResponse::Conflict().json(Error { message: e.0 });
     }
+
+    let ip = NetDevIp::new(ipv4, ipv6);
+    let netdev_result = manager
+        .netdev
+        .set_ip(&device.device_name, &ip)
+        .and_then(|_| manager.netdev.up(&device.device_name));
+    if let Err(e) = netdev_result {
+        return HttpResponse::BadRequest().json(Error { message: e.0 });
+    }
+
+    let Ok(d) = wg_result else {
+        return HttpResponse::InternalServerError().json(Error {
+            message: "system error".to_string(),
+        });
+    };
+
+    let dev = CreateDeviceResponse {
+        device_name: d.name,
+        port: d.port,
+        ip_addresses: DeviceIpAddr {
+            ipv4: ip.ipv4_str(),
+            ipv6: ip.ipv6_str(),
+        },
+        private_key: d.private_key,
+        public_key: d.public_key,
+    };
+    HttpResponse::Created().json(dev)
 }
 
 #[utoipa::path(
@@ -80,30 +134,48 @@ async fn create_device(
     ),
     responses(
         (status = 200, description = "device found", body = DetailDeviceResponse),
-        (status = 404, description = "device not found")
+        (status = 400, description = "validation error", body = Error),
+        (status = 404, description = "device not found"),
+        (status = 500, description = "system error", body = Error),
     )
 )]
 #[get("/devices/{dev}")]
 async fn get_device(tm: web::Data<TunnelManager>, path: web::Path<String>) -> impl Responder {
     let dev_name = path.into_inner();
-    let manager = tm.get_ref();
-    let result = manager.wireguard.get_device(&dev_name);
-    match result {
-        Err(e) => HttpResponse::NotFound().json(Error { message: e.0 }),
-        Ok(d) => {
-            let out = DetailDeviceResponse {
-                device_name: d.name,
-                port: d.port,
-                ip_addresses: DeviceIpAddr {
-                    ipv4: None,
-                    ipv6: None,
-                },
-                public_key: d.public_key,
-                peers: d.peers,
-            };
-            HttpResponse::Ok().json(out)
-        }
+    if dev_name.len() > DEVICE_NAME_MAX_LEN {
+        return HttpResponse::BadRequest().json(Error {
+            message: "device name must be at most 15 characters".to_owned(),
+        });
     }
+
+    let manager = tm.get_ref();
+    let wg_result = manager.wireguard.get_device(&dev_name);
+    if let Err(e) = wg_result {
+        return HttpResponse::NotFound().json(Error { message: e.0 });
+    }
+
+    let netdev_result = manager.netdev.get_ip(&dev_name);
+    if let Err(e) = netdev_result {
+        return HttpResponse::InternalServerError().json(Error { message: e.0 });
+    }
+
+    let (Ok(d), Ok(ip)) = (wg_result, netdev_result) else {
+        return HttpResponse::InternalServerError().json(Error {
+            message: "system error".to_string(),
+        });
+    };
+
+    let out = DetailDeviceResponse {
+        device_name: d.name,
+        port: d.port,
+        ip_addresses: DeviceIpAddr {
+            ipv4: ip.ipv4_str(),
+            ipv6: ip.ipv6_str(),
+        },
+        public_key: d.public_key,
+        peers: d.peers,
+    };
+    HttpResponse::Ok().json(out)
 }
 
 #[utoipa::path(
@@ -115,12 +187,19 @@ async fn get_device(tm: web::Data<TunnelManager>, path: web::Path<String>) -> im
     ),
     responses(
         (status = 204, description = "successfully deleted"),
+        (status = 400, description = "validation error", body = Error),
         (status = 404, description = "device not found")
     )
 )]
 #[delete("/devices/{dev}")]
 async fn delete_device(tm: web::Data<TunnelManager>, path: web::Path<String>) -> impl Responder {
     let dev_name = path.into_inner();
+    if dev_name.len() > DEVICE_NAME_MAX_LEN {
+        return HttpResponse::BadRequest().json(Error {
+            message: "device name must be at most 15 characters".to_owned(),
+        });
+    }
+
     let manager = tm.get_ref();
     match manager.wireguard.delete_device(&dev_name) {
         Err(e) => HttpResponse::NotFound().json(Error { message: e.0 }),
--- wghttp/src/routes/peers.rs
+++ wghttp/src/routes/peers.rs
@@ -1,3 +1,4 @@
+use crate::helpers::*;
 use crate::models::errors::Error;
 use crate::models::peers::*;
 use crate::services::TunnelManager;
@@ -12,12 +13,19 @@ use actix_web::{HttpResponse, Responder, delete, get, post, web};
     ),
     responses(
         (status = 200, description = "list of peers for given wireguard device", body = [ListPeerResponse]),
+        (status = 400, description = "validation error", body = Error),
         (status = 404, description = "device not found")
     )
 )]
 #[get("/devices/{dev}/peers")]
 async fn list_peers(tm: web::Data<TunnelManager>, path: web::Path<String>) -> impl Responder {
     let dev_name = path.into_inner();
+    if dev_name.len() > DEVICE_NAME_MAX_LEN {
+        return HttpResponse::BadRequest().json(Error {
+            message: "device name must be at most 15 characters".to_owned(),
+        });
+    }
+
     let manager = tm.get_ref();
     let peers = manager.wireguard.list_peers(&dev_name);
     match peers {
@@ -60,9 +68,19 @@ async fn create_peer(
     path: web::Path<String>,
     peer: web::Json<CreatePeerRequest>,
 ) -> impl Responder {
-    // TODO: input validation
-
     let dev_name = path.into_inner();
+    if dev_name.len() > DEVICE_NAME_MAX_LEN {
+        return HttpResponse::BadRequest().json(Error {
+            message: "device name must be at most 15 characters".to_owned(),
+        });
+    }
+
+    if let Err(e) = validate_ip_list(&peer.allowed_ips) {
+        return HttpResponse::BadRequest().json(Error {
+            message: e,
+        })
+    }
+
     let manager = tm.get_ref();
     let ips: Vec<&str> = peer.allowed_ips.iter().map(|s| s.as_str()).collect();
     let result = manager
@@ -93,6 +111,7 @@ async fn create_peer(
     ),
     responses(
         (status = 204, description = "successfully deleted"),
+        (status = 400, description = "validation error", body = Error),
         (status = 404, description = "device or peer not found")
     )
 )]
@@ -102,6 +121,18 @@ async fn delete_peer(
     path: web::Path<(String, String)>,
 ) -> impl Responder {
     let (dev, public_key) = path.into_inner();
+    if dev.len() > DEVICE_NAME_MAX_LEN {
+        return HttpResponse::BadRequest().json(Error {
+            message: "device name must be at most 15 characters".to_owned(),
+        });
+    }
+
+    if public_key.len() != PUBKEY_MAX_LEN {
+        return HttpResponse::BadRequest().json(Error {
+            message: "public key must be 44 characters".to_owned(),
+        });
+    }
+
     let manager = tm.get_ref();
     match manager.wireguard.delete_peer(&dev, &public_key) {
         Err(e) => HttpResponse::NotFound().json(Error { message: e.0 }),
