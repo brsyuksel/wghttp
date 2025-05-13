7bc1332e42f17dea5adc1e263d343ae7202b1a96
Baris Yuksel <brsyuksel@gmail.com>
2025-05-13 22:43:39 +0000

    NetDevIp new fn from ipaddr

--- domain/src/models.rs
+++ domain/src/models.rs
@@ -37,7 +37,7 @@ pub mod wg {
 }
 
 pub mod netdev {
-    use std::net::{Ipv4Addr, Ipv6Addr};
+    use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
     #[derive(Debug)]
     pub struct NetDevError(pub String);
@@ -54,5 +54,32 @@ pub mod netdev {
     pub struct NetDevIp {
         pub ipv4: Option<(Ipv4Addr, u8)>,
         pub ipv6: Option<(Ipv6Addr, u8)>,
+    }
+
+    impl NetDevIp {
+        pub fn new(ipv4_opt: Option<(IpAddr, u8)>, ipv6_opt: Option<(IpAddr, u8)>) -> Self {
+            let ipv4 = match ipv4_opt {
+                Some((IpAddr::V4(addr), p)) => Some((addr, p)),
+                _ => None,
+            };
+
+            let ipv6 = match ipv6_opt {
+                Some((IpAddr::V6(addr), p)) => Some((addr, p)),
+                _ => None,
+            };
+
+            Self {
+                ipv4: ipv4,
+                ipv6: ipv6,
+            }
+        }
+
+        pub fn ipv4_str(&self) -> Option<String> {
+            self.ipv4.map(|t| format!("{}/{}", t.0.to_string(), t.1))
+        }
+
+        pub fn ipv6_str(&self) -> Option<String> {
+            self.ipv6.map(|t| format!("{}/{}", t.0.to_string(), t.1))
+        }
     }
 }
