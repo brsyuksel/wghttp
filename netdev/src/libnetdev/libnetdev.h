396abe6b8ecb6a3b5af92b12de268519671b2d65
Baris Yuksel <brsyuksel@gmail.com>
2025-06-04 15:57:08 +0000

    chore: clang format

--- netdev/src/libnetdev/libnetdev.h
+++ netdev/src/libnetdev/libnetdev.h
@@ -1,7 +1,7 @@
 /**
  * @file libnetdev.h
  * @brief Public API for interacting with network devices and their IP configurations.
- * 
+ *
  * This library provides functions to get and set IP addresses, netmasks, and device states
  * for network interfaces. It abstracts the underlying system calls and provides a simple
  * interface for managing network devices programmatically.
@@ -12,8 +12,8 @@
 
 #include <arpa/inet.h>
 
-// Maximum length for ip prefix addition in CIDR notation. Since Ipv6 can have a prefix length of up to 3 digits,
-// we define a maximum length of 4 to accommodate the prefix and the null terminator.
+// Maximum length for ip prefix addition in CIDR notation. Since Ipv6 can have a prefix length of up
+// to 3 digits, we define a maximum length of 4 to accommodate the prefix and the null terminator.
 #define IP_PREFIX_MAXLEN 4
 
 // Maximum length for an IPv4 or IPv6 address string in CIDR notation
@@ -40,7 +40,7 @@ typedef enum {
 
 /**
  * @brief Represents an IP configuration for a network device.
- * 
+ *
  * Contains both IPv4 and IPv6 addresses in CIDR notation.
  */
 typedef struct libnetdev_ip {
@@ -50,7 +50,7 @@ typedef struct libnetdev_ip {
 
 /**
  * @brief Retrieves the IP configuration for a given network device.
- * 
+ *
  * @param device_name Name of the network device (e.g., "eth0", "wg0")
  * @param ip Output pointer to the libnetdev_ip structure that will be filled with the device's IP
  *           configuration.
@@ -60,10 +60,10 @@ int libnetdev_get_ip(const char *device_name, libnetdev_ip **ip);
 
 /**
  * @brief Sets the IP configuration for a given network device.
- * 
+ *
  * This function sets both the IPv4 and IPv6 addresses in CIDR notation. If an address is not set,
  * it should be an empty string.
- * 
+ *
  * @param device_name Name of the network device (e.g., "eth0", "wg0")
  * @param ip Pointer to the libnetdev_ip structure containing the new IP configuration.
  * @return 0 on success, non-zero on failure.
@@ -72,7 +72,7 @@ int libnetdev_set_ip(const char *device_name, libnetdev_ip *ip);
 
 /**
  * @brief Retrieves the netmask for a given network device.
- * 
+ *
  * @param device_name Name of the network device (e.g., "eth0", "wg0")
  * @return 0 on success, non-zero on failure.
  */
@@ -80,7 +80,7 @@ int libnetdev_up(const char *device_name);
 
 /**
  * @brief Frees the memory allocated for a libnetdev_ip structure.
- * 
+ *
  * @param ip Pointer to the libnetdev_ip structure to be freed
  */
 void libnetdev_free_ip(libnetdev_ip *ip);
